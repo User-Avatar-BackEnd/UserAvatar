445e15a5133680bd4a730c9803a3ffe9257dfa52
DaniilPanasenko <daniil.panasenko@nure.ua>
2021-03-19 00:31:56 +0200

    db context

---
+++ UserAvatar.DAL/Context/UserAvatarContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserAvatar.DAL.Context
+{
+    public class UserAvatarContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public ProductManagerContext(DbContextOptions<ProductManagerContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.LogTo(Console.WriteLine);
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=qwerty");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .ToTable("Products");
+        }
+    }
+}
