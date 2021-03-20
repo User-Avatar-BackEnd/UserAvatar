c94b48ea5d51129883e081fcc225bfec2d3ab589
Ihor Volokhovych <igorvolokhovych@gmail.com>
2021-03-20 20:42:55 +0200

    addcors

---
+++ UserAvatar.API/Controllers/ColumnController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UserAvatar.API.Contracts;
+using UserAvatar.BLL.Services.Interfaces;
+
+namespace UserAvatar.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/columns")]
+    public class ColumnController : ControllerBase
+    {
+        private readonly IColumnService _columnService;
+        public ColumnController(IColumnService columnService)
+        {
+            _columnService = columnService;
+        }
+
+        [HttpPost]
+        [Route("/create")]
+        public IActionResult CreateColumn(ColumnRequest columnRequest)
+        {
+            _columnService
+                .Create(columnRequest.BoardOrColumnId,columnRequest.Title);
+            return Ok();
+        }
+        
+        [HttpPatch]
+        [Route("/modify")]
+        public IActionResult UpdateColumn(ColumnRequest columnRequest)
+        {
+            throw new NotImplementedException();
+        }
+        
+        [HttpDelete]
+        [Route("/delete")]
+        public IActionResult DeleteColumn(ColumnRequest columnRequest)
+        {
+            _columnService.Delete(columnRequest.BoardOrColumnId);
+            return Ok();
+        }
+        
+        [HttpGet]
+        [Route("/change_position/")]
+        public IActionResult ChangeColumnPosition([FromHeader] int columnId,
+            [FromQuery] int positionIndex)
+        {
+            _columnService.ChangePosition(columnId,positionIndex);
+            return Ok();
+        }
+    }
+}
--- UserAvatar.API/Startup.cs
+++ UserAvatar.API/Startup.cs
@@ -12,9 +12,11 @@ using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using UserAvatar.API.Options;
 using UserAvatar.BLL.Services;
+using UserAvatar.BLL.Services.Interfaces;
 using UserAvatar.DAL.Context;
 using UserAvatar.DAL.Entities;
 using UserAvatar.DAL.Storages;
+using UserAvatar.DAL.Storages.Interfaces;
 
 namespace UserAvatar.API
 {
@@ -77,10 +79,13 @@ namespace UserAvatar.API
 
             services.AddTransient<UserStorage>();
             services.AddTransient<BoardStorage>();
+            services.AddTransient<IColumnStorage, ColumnStorage>();
+            
 
             services.AddTransient<IAuthService, AuthService>();
             services.AddTransient<IBoardService, BoardService>();
-            services.AddTransient<ITaskService, TaskService>();
+            services.AddTransient<IColumnService, ColumnService>();
+            //services.AddTransient<ITaskService, TaskService>();
 
             services.AddControllers();
             services.AddSwaggerGen(options =>
@@ -128,11 +133,16 @@ namespace UserAvatar.API
                 EnsureAdminCreated(context);
             }
 
-             app.UseDeveloperExceptionPage();
-             app.UseSwagger();
-             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "UserAvatar v1"));
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "UserAvatar v1"));
 
             app.UseRouting();
+            
+            // Use cors
+            app.UseCors(builder => builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()); 
 
             app.UseAuthentication();
             app.UseAuthorization();
@@ -143,11 +153,11 @@ namespace UserAvatar.API
             });
         }
 
-        private void EnsureAdminCreated(UserAvatarContext context)
+        private static void EnsureAdminCreated(UserAvatarContext context)
         {
-
-            var testBlog = context.Users.FirstOrDefault(x=> x.Login == "admin" && x.PasswordHash == "admin");
-            if (testBlog == null)
+            var adminUser = context.Users.Any(x=> x.Email == "admin@admin.com" 
+                                                  && x.Login == "admin");
+            if (!adminUser)
             {
                 context.Users.Add(new User
                 {
