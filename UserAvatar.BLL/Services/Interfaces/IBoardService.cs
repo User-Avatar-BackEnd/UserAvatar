d39e6c9fba2a24371b6267b98fcedc8f3621a0d0
Karyna <k.bilotska@gmail.com>
2021-03-21 19:11:38 +0200

    Debugged

--- UserAvatar.BLL/Services/Interfaces/IBoardService.cs
+++ UserAvatar.BLL/Services/Interfaces/IBoardService.cs
@@ -7,12 +7,12 @@ namespace UserAvatar.Bll.Services.Interfaces
     {
         System.Threading.Tasks.Task<IEnumerable<BoardModel>> GetAllBoardsAsync(int userId);
 
-        System.Threading.Tasks.Task<bool> CreateBoardAsync(int userId, string title);
+        System.Threading.Tasks.Task CreateBoardAsync(int userId, string title);
 
         System.Threading.Tasks.Task<BoardModel> GetBoardAsync(int userId, int boardId);
 
-        System.Threading.Tasks.Task<bool> RenameBoardAsync(int userId, int boardId, string title);
+        System.Threading.Tasks.Task RenameBoardAsync(int userId, int boardId, string title);
 
-        System.Threading.Tasks.Task<bool> DeleteBoardAsync(int userId, int boardId);
+        System.Threading.Tasks.Task DeleteBoardAsync(int userId, int boardId);
     }
 }
