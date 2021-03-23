145c74eb0b3a00a08a35403c1d4a067fa294d264
DaniilPanasenko <daniil.panasenko.work@gmail.com>
2021-03-23 19:54:20 +0200

    card by routing

--- UserAvatar.Bll.TaskManager/Services/CardService.cs
+++ UserAvatar.Bll.TaskManager/Services/CardService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using AutoMapper;
+using UserAvatar.Bll.TaskManager.Infrastructure;
 using UserAvatar.Bll.TaskManager.Models;
 using UserAvatar.Bll.TaskManager.Services.Interfaces;
 using UserAvatar.Dal.Entities;
@@ -12,24 +13,34 @@ namespace UserAvatar.Bll.TaskManager.Services
     {
         private readonly ICardStorage _cardStorage;
         private readonly IBoardStorage _boardStorage;
+        private readonly IColumnStorage _columnStorage;
         private readonly IMapper _mapper;
 
-        public CardService(ICardStorage cardStorage, IMapper mapper, IBoardStorage boardStorage)
+        public CardService(ICardStorage cardStorage, IMapper mapper, IBoardStorage boardStorage, IColumnStorage columnStorage)
         {
             _cardStorage = cardStorage;
             _boardStorage = boardStorage;
             _mapper = mapper;
+            _columnStorage = columnStorage;
         }
 
-        public async Task<CardModel> CreateCardAsync(string title, int columnId, int userId)
+        public async Task<Result<CardModel>> CreateCardAsync(string title, int boardId, int columnId, int userId)
         {
-            //var boardId = _cardStorage.GetBoardId(cardId);
-            //if (_boardStorage.IsUsersBoard(userId, boardId)) return null;//isUserBoard
+            var column = await _columnStorage.GetColumnByIdAsync(columnId);
+            if(column==null || column.BoardId != boardId)
+            {
+                return new Result<CardModel>(ResultCode.NotFound);
+            }
 
-            if (string.IsNullOrEmpty(title)) return null;
-            if (columnId < 1) return null;
+            if (await _boardStorage.IsUserBoardAsync(userId, boardId))
+            {
+                return new Result<CardModel>(ResultCode.Forbidden);
+            }
 
-            if (await _cardStorage.GetCardsCountInColumnAsync(columnId) > 100) throw new Exception();
+            if (await _cardStorage.GetCardsCountInColumnAsync(columnId) > 100)
+            {
+                return new Result<CardModel>(ResultCode.MaxColumnCount);
+            }
 
             var card = new Card
             {
@@ -40,21 +51,28 @@ namespace UserAvatar.Bll.TaskManager.Services
                 ModifiedAt = DateTime.UtcNow,
                 IsDeleted = false,
                 IsHidden = false,
-                ColumnId = columnId
+                ColumnId = columnId,
+                ModifiedBy = userId
             };
 
             card = await _cardStorage.CreateAsync(card);
             var cardModel = _mapper.Map<Card, CardModel>(card);
-            return cardModel;
+
+            return new Result<CardModel>(cardModel);
         }
 
-        public async Task UpdateCardAsync(CardModel cardModel, int columnId, int? responsibleId, int userId)
+        public async Task<int> UpdateCardAsync(CardModel cardModel, int boardId, int columnId, int? responsibleId, int userId)
         {
-            var boardId = await _cardStorage.GetBoardIdAsync(cardModel.Id);
-
-            if (!await _boardStorage.IsUserBoardAsync(userId, boardId)) throw new Exception();
-
             var card = await _cardStorage.GetByIdAsync(cardModel.Id);
+            if (card == null)
+            {
+                return ResultCode.NotFound;
+            }
+
+            if (!await _boardStorage.IsUserBoardAsync(userId, boardId))
+            {
+                return ResultCode.Forbidden;
+            }
 
             card.Title = card.Title;
             card.Description = cardModel.Description;
@@ -65,28 +83,40 @@ namespace UserAvatar.Bll.TaskManager.Services
             card.Priority = cardModel.Priority;
 
             await _cardStorage.UpdateAsync(card);
+            return ResultCode.Success;
         }
 
-        public async Task<CardModel> GetByIdAsync(int cardId, int userId)
+        public async Task<Result<CardModel>> GetByIdAsync(int boardId, int cardId, int userId)
         {
-            var boardId = await _cardStorage.GetBoardIdAsync(cardId);
-
-            if (!await _boardStorage.IsUserBoardAsync(userId, boardId)) throw new Exception();
-
             var card = await _cardStorage.GetByIdAsync(cardId);
+            if (card == null)
+            {
+                return new Result<CardModel>(ResultCode.NotFound);
+            }
 
-            return card == null ? null : _mapper.Map<Card, CardModel>(card);
+            if (!await _boardStorage.IsUserBoardAsync(userId, boardId))
+            {
+                return new Result<CardModel>(ResultCode.Forbidden);
+            }
+
+            return new Result<CardModel>(_mapper.Map<Card, CardModel>(card));
         }
 
-        public async Task DeleteCardAsync(int cardId, int userId)
+        public async Task<int> DeleteCardAsync(int boardId, int cardId, int userId)
         {
-            if (await _cardStorage.GetByIdAsync(cardId) == null)
-                return;
-            var boardId = await _cardStorage.GetBoardIdAsync(cardId);
+            var card = await _cardStorage.GetByIdAsync(cardId);
+            if (card == null)
+            {
+                return ResultCode.NotFound;
+            }
 
-            if (!await _boardStorage.IsUserBoardAsync(userId, boardId)) throw new Exception();
+            if (!await _boardStorage.IsUserBoardAsync(userId, boardId))
+            {
+                return ResultCode.Forbidden;
+            }
 
             await _cardStorage.DeleteAsync(cardId);
+            return ResultCode.Success;
         }
     }
 }
