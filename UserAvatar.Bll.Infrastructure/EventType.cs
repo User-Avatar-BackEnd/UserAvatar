a0c2b07b58a447806fe7fd44a17ebc2edc100389
DaniilPanasenko <daniil.panasenko.work@gmail.com>
2021-03-25 01:56:21 +0200

    add event type

---
+++ UserAvatar.Bll.Infrastructure/EventType.cs
@@ -0,0 +1,23 @@
+﻿namespace UserAvatar.Bll.Infrastructure
+{
+    public static class EventType
+    {
+        public const string Registration = "Registration";
+
+        public const string Login = "Login";
+
+        public const string Logout = "Logout";
+
+        public const string CreateBoard = "Create board";
+
+        public const string SendInvite = "Send invite";
+
+        public const string CreateCardOnOwnBoard = "Create card on own board";
+
+        public const string CreateCardOnAlienBoard = "Create card on alien board";
+
+        public const string ChangeCardStatusOnOwnBoard = "Change card status on own board";
+
+        public const string ChangeCardStatusOnAlienBoard = "Change card status on alien board";
+    }
+}
