a7f6057b8b81c8261803e4a329301f57dee094dd
Ihor <igorvolokhovych@gmail.com>
2021-03-24 02:58:23 +0200

    Update because of conflicts

--- README.md
+++
@@ -1,2 +0,0 @@
-# UserAvatar
-User avatar project
--- UserAvatar.Api/Contracts/Requests/CreateColumnRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UserAvatar.Api.Contracts.Requests
-{
-    public class CreateColumnRequest
-    {
-        public int BoardId { get; set; }
-        public string Title { get; set; }
-    }
-}
