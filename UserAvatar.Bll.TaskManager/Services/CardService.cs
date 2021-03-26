a509a746a7a1a85a562c6dc9c2d6d661f54cef35
DaniilPanasenko <daniil.panasenko.work@gmail.com>
2021-03-26 19:12:42 +0200

    card max count fix

--- UserAvatar.Bll.TaskManager/Services/CardService.cs
+++ UserAvatar.Bll.TaskManager/Services/CardService.cs
@@ -53,7 +53,7 @@ namespace UserAvatar.Bll.TaskManager.Services
 
             if (await _cardStorage.GetCardsCountInColumnAsync(columnId) > _limitations.MaxCardCount)
             {
-                return new Result<CardModel>(ResultCode.MaxColumnCount);
+                return new Result<CardModel>(ResultCode.MaCardCount);
             }
 
             var card = new Card
