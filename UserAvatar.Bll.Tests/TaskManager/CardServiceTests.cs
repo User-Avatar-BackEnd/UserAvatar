6a864fbf7eee6687d032183b7a63d05cd2eb8815
Karyna <k.bilotska@gmail.com>
2021-03-27 04:08:49 +0200

    Added board service tests

--- UserAvatar.Bll.Tests/TaskManager/CardServiceTests.cs
+++ UserAvatar.Bll.Tests/TaskManager/CardServiceTests.cs
@@ -9,79 +9,79 @@ using Xunit;
 
 namespace UserAvatar.Bll.Tests.TaskManager
 {
-    public class CardServiceTests
-    {
-        private readonly ICardStorage _cardStorage;
-        private readonly IBoardStorage _boardStorage;
-        private readonly IColumnStorage _columnStorage;
-        private readonly LimitationOptions _limitations;
-        private readonly IMapper _mapper;
-        private readonly IBoardChangesService _boardChangesService;
+    //public class CardServiceTests
+    //{
+    //    private readonly ICardStorage _cardStorage;
+    //    private readonly IBoardStorage _boardStorage;
+    //    private readonly IColumnStorage _columnStorage;
+    //    private readonly LimitationOptions _limitations;
+    //    private readonly IMapper _mapper;
+    //    private readonly IBoardChangesService _boardChangesService;
 
-        public CardServiceTests(IMapper mapper)
-        {
-            _mapper = mapper;
-        }
+    //    public CardServiceTests(IMapper mapper)
+    //    {
+    //        _mapper = mapper;
+    //    }
 
-        public async Task<Result<CardModel>> CreateCardAsync(string title,
-    int boardId, int columnId, int userId)
-        {
-            var column = await _columnStorage.GetColumnByIdAsync(columnId);
+    //    public async Task<Result<CardModel>> CreateCardAsync(string title,
+    //int boardId, int columnId, int userId)
+    //    {
+    //        var column = await _columnStorage.GetColumnByIdAsync(columnId);
 
-            if (column == null || column.BoardId != boardId)
-            {
-                return new Result<CardModel>(ResultCode.NotFound);
-            }
+    //        if (column == null || column.BoardId != boardId)
+    //        {
+    //            return new Result<CardModel>(ResultCode.NotFound);
+    //        }
 
-            if (!await _boardStorage.IsUserBoardAsync(userId, boardId))
-            {
-                return new Result<CardModel>(ResultCode.Forbidden);
-            }
+    //        if (!await _boardStorage.IsUserBoardAsync(userId, boardId))
+    //        {
+    //            return new Result<CardModel>(ResultCode.Forbidden);
+    //        }
 
-            if (await _cardStorage.GetCardsCountInColumnAsync(columnId) >= _limitations.MaxCardCount)
-            {
-                return new Result<CardModel>(ResultCode.MaxCardCount);
-            }
+    //        if (await _cardStorage.GetCardsCountInColumnAsync(columnId) >= _limitations.MaxCardCount)
+    //        {
+    //            return new Result<CardModel>(ResultCode.MaxCardCount);
+    //        }
 
-            var card = new Card
-            {
-                Title = title,
-                Description = "",
-                OwnerId = userId,
-                CreatedAt = DateTimeOffset.UtcNow,
-                ModifiedAt = DateTimeOffset.UtcNow,
-                ColumnId = columnId,
-                ModifiedBy = userId
-            };
+    //        var card = new Card
+    //        {
+    //            Title = title,
+    //            Description = "",
+    //            OwnerId = userId,
+    //            CreatedAt = DateTimeOffset.UtcNow,
+    //            ModifiedAt = DateTimeOffset.UtcNow,
+    //            ColumnId = columnId,
+    //            ModifiedBy = userId
+    //        };
 
-            card = await _cardStorage.CreateAsync(card);
-            var cardModel = _mapper.Map<Card, CardModel>(card);
+    //        card = await _cardStorage.CreateAsync(card);
+    //        var cardModel = _mapper.Map<Card, CardModel>(card);
 
-            var board = await _boardStorage.GetBoardAsync(boardId);
-            var eventType = board.OwnerId == userId ?
-                EventType.CreateCardOnOwnBoard :
-                EventType.CreateCardOnAlienBoard;
+    //        var board = await _boardStorage.GetBoardAsync(boardId);
+    //        var eventType = board.OwnerId == userId ?
+    //            EventType.CreateCardOnOwnBoard :
+    //            EventType.CreateCardOnAlienBoard;
 
-            _boardChangesService.DoChange(boardId, userId);
+    //        _boardChangesService.DoChange(boardId, userId);
 
-            return new Result<CardModel>(cardModel, eventType);
-        }
+    //        return new Result<CardModel>(cardModel, eventType);
+    //    }
 
-        [Fact]
-        public void CreateCard_If_Board_Does_Not_Exist_Returns_ResultCode_NotFound()
-        {
-            // Arrange
-            var mock = new Mock<IColumnStorage>();
-            mock.Setup(x=>x.GetColumnByIdAsync()).Returns(GetTestUsers());
-            var controller = new HomeController(mock.Object);
+    //    [Fact]
+    //    public void CreateCard_If_Board_Does_Not_Exist_Returns_ResultCode_NotFound()
+    //    {
+    //        // Arrange
+    //        var mock = new Mock<IColumnStorage>();
+    //        mock.Setup(x=>x.GetColumnByIdAsync()).Returns(GetTestUsers());
+    //        var controller = new HomeController(mock.Object);
 
-            // Act
-            var result = controller.Index();
+    //        // Act
+    //        var result = controller.Index();
 
-            // Assert
-            var viewResult = Assert.IsType<ViewResult>(result);
-            var model = Assert.IsAssignableFrom<IEnumerable<User>>(viewResult.Model);
-            Assert.Equal(GetTestUsers().Count, model.Count());
-        }
-    }
+    //        // Assert
+    //        var viewResult = Assert.IsType<ViewResult>(result);
+    //        var model = Assert.IsAssignableFrom<IEnumerable<User>>(viewResult.Model);
+    //        Assert.Equal(GetTestUsers().Count, model.Count());
+    //    }
+    //}
 }
